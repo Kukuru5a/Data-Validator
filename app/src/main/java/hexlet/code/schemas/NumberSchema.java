f7412265c8246a1fc2e828da3bee8d339b7d30c0
kukuru5a <romanzag@icloud.com>
2023-08-03 20:46:12 +0600

    10th commit. Replaced "instanceof" with comparing of lambda phrase

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 public class NumberSchema extends BaseSchema {
     public final NumberSchema required() {
         this.isRequired = true;
-        Predicate<Object> required = s -> s instanceof Integer;
+        Predicate<Object> required = s -> (s != null && (int)s > 0);
         super.addPred(required);
         return this;
     }
